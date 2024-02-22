229acc31554e1dc20427fc389b704a955730c36b
Nurettin KAYNAR <nurettin.kaynar@ardgrup.com.tr>
2024-02-22 18:28:20 +0300

    spalsh screen eklendi

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,6 @@
-import { Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { Stack, SplashScreen } from "expo-router";
 import { useFonts } from "expo-font";
-// import * as SplashScreen from "expo-splash-screen";
-
-// SplashScreen.preventAutoHideAsync();
 
 export const unstable_settings = {
   // Ensure any route can link back to `/`
@@ -16,15 +14,22 @@ const Layout = () => {
     DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
   });
 
+  useEffect(() => {
+    if (!fontsLoaded) {
+      return; // Exit early if fonts are not loaded
+    }
+    SplashScreen.preventAutoHideAsync(); // Call preventAutoHideAsync within a useEffect hook
+  }, [fontsLoaded]); // Ensure useEffect runs whenever fontsLoaded changes
+
   if (!fontsLoaded) {
-    return null;
+    return null; // Return null if fonts are not loaded yet
   }
 
   return (
     <Stack initialRouteName="Home">
       <Stack.Screen name="Home" />
     </Stack>
-  )
+  );
 };
 
 export default Layout;
